eda60d948973abef3b3186523c011aababb942c0
R-Kiselev <sputnavik@gmail.com>
2024-09-09 23:41:05 +0300

    Created flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+import settings
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+
+db = SQLAlchemy(app)
+api = Api(app)
+
+# Add resources
+
+if __name__ == __main__:
+    app.run(debug= True)
