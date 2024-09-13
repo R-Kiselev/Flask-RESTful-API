49a6a764763c277be988ef9f76752be5b74a54b1
R-Kiselev <sputnavik@gmail.com>
2024-09-13 16:15:59 +0300

    Add: GET Bank method

--- endpoints/banks/resource.py
+++ endpoints/banks/resource.py
@@ -8,17 +8,41 @@ bank_fields = {
     'name': fields.String
 }
 
+bank_list_fields = {
+    'total': fields.Integer,
+    'banks': fields.List(fields.Nested(bank_fields))
+}
+
 args_parser = reqparse.RequestParser()
 args_parser.add_argument('id', type=int, help="Bank id should be integer")
 args_parser.add_argument('name', type=str, required=True, help="Bank name cannot be empty")
 
 class BanksResource(Resource):
-    @marshal_with(bank_fields)
     def get(self, bank_id = None):
         if bank_id:
             bank = Bank.query.filter_by(id=bank_id).first()
-            return bank
-    
+            return marshal(bank, bank_fields)
+        else:
+            request_args = request.args.to_dict()
+            limit = request_args.get('limit', 0)
+            offset = request_args.get('offset', 0)
+
+            request_args.pop('limit', None)
+            request_args.pop('offset', None)
+
+            banks = Bank.query.filter_by(**request_args)
+
+            if limit:
+                banks = banks.limit(limit)
+            if offset:
+                banks = banks.offset(offset)
+            
+            banks = banks.all()
+            return marshal({
+                'total': len(banks),
+                'banks': marshal([bank for bank in banks], bank_fields)
+            }, bank_list_fields)
+
     @marshal_with(bank_fields)
     def post(self):      
         args = args_parser.parse_args()
