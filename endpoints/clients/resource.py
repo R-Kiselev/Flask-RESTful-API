e392a2431a69ddda5e4fff4883679387a1fbfc06
R-Kiselev <sputnavik@gmail.com>
2024-09-16 21:33:34 +0300

    Add : Using virtual base class for defining resource class

---
+++ endpoints/clients/resource.py
@@ -0,0 +1,23 @@
+from flask_restful import reqparse, fields
+from endpoints.BaseResource import BaseResource
+from .model import Client
+
+class ClientResource(BaseResource):
+    model = Client
+    
+    item_fields = {
+        'id': fields.Integer,
+        'name': fields.String,
+        'social_status_id': fields.Integer
+    }
+
+    item_list_fields = {
+        'table_name': fields.String,
+        'total': fields.Integer,
+        'items': fields.List(fields.Nested(item_fields))
+    }
+
+    args_parser = reqparse.RequestParser()
+    args_parser.add_argument('id', type=int, help="Client id should be integer")
+    args_parser.add_argument('name', type=str, required=True, help="Client name cannot be empty")  
+    args_parser.add_argument('social_status_id', type=int, required = True, help="Social status id should be integer")
---
+++ endpoints/social_statuses/resource.py
@@ -0,0 +1,21 @@
+from flask_restful import reqparse, fields
+from endpoints.BaseResource import BaseResource
+from .model import SocialStatus
+
+class SocialStatusResource(BaseResource):
+    model = SocialStatus
+    
+    item_fields = {
+        'id': fields.Integer,
+        'name': fields.String
+    }
+
+    item_list_fields = {
+        'table_name': fields.String,
+        'total': fields.Integer,
+        'items': fields.List(fields.Nested(item_fields))
+    }
+
+    args_parser = reqparse.RequestParser()
+    args_parser.add_argument('id', type=int, help="Social status id should be integer")
+    args_parser.add_argument('name', type=str, required=True, help="Social status name cannot be empty")    
