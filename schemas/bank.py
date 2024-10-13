b7362fd6bab242e621481522c5f033a4626b5467
R-Kiselev <sputnavik@gmail.com>
2024-10-13 16:49:03 +0300

    feat: added bank marshmallow schema

--- schemas/bank.py
+++ schemas/bank.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
 
 class BankSchema(Schema):
-    id = fields.Int(dump_only = True)
+    id = fields.Int(dump_only= True)
     name = fields.Str(required= True)
+    
