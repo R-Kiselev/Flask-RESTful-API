edb97079f3b145e970e8369e3fb74b64a86540e8
R-Kiselev <sputnavik@gmail.com>
2024-10-13 22:49:15 +0300

    refactor: changed resources names, structure, urls

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, Blueprint, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 import db_settings as database
 from flask_marshmallow import Marshmallow
+from marshmallow import ValidationError
 
 app = Flask(__name__)
 app.config['WTF_CSRF_ENABLED'] = True
@@ -10,64 +11,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = database.SQLALCHEMY_TRACK_MODIFICATIONS
 
 database.db.init_app(app)
-api = Api(app)
+blueprint = Blueprint("api", __name__)
+
+api = Api(blueprint)
 ma = Marshmallow(app)
 
-'''
-
-Филиалы банка:
-
-GET /banks/branches/{id} — получить информацию о конкретном филиале.
-GET /banks/{id}/branches — получить список филиалов конкретного банка.
-
-Счета:
-
-GET /banks/accounts/{id} — получить информацию о конкретном счете.
-GET /banks/{id}/accounts — получить все счета банка.
-
-GET /clients/{id}/accounts — получить список счетов конкретного клиента.
-GET /clients/{id}/accounts/{id} — получить счет конкретного клиента.
-
-Карты:
-
-GET /accounts/cards/{id} — получить список карт для конкретного счета.
-GET /accounts/{id}/cards — получить список карт для конкретного счета.
-
-
-Советы:
-1) Создание кстати хорошо сделать через /banks/id/accounts
-2) Главное логику вынести в сервис, чтобы не дублировать код
-3) Использовать marshmallow вместо reqparser  
-'''
+@blueprint.errorhandler(ValidationError)
+def handle_marshmallow_error(e):
+    return jsonify(e.messages), 400
 
 # Add resources
-from resources.banks import BankResource
-api.add_resource(BankResource,'/banks','/banks/<int:id>')
+from resources.banks import BankObjectResource, BankListResource
+api.add_resource(BankListResource, '/banks')
+api.add_resource(BankObjectResource, '/banks/<int:id>')
 
-from resources.cities import CityResource
-api.add_resource(CityResource,'/cities','/cities/<int:id>')
+from resources.cities import CityObjectResource, CityListResource
+api.add_resource(CityListResource, '/cities')
+api.add_resource(CityObjectResource, '/cities/<int:id>')
 
-from resources.social_statuses import SocialStatusResource
-api.add_resource(SocialStatusResource,'/social_statuses','/social-statuses/<int:id>')
+from resources.social_statuses import SocialStatusObjectResource, SocialStatusListResource
+api.add_resource(SocialStatusListResource,'/social-statuses')
+api.add_resource(SocialStatusObjectResource,'/social-statuses/<int:id>')
 
-from resources.clients import ClientResource
-api.add_resource(ClientResource,'/clients','/clients/<int:id>')
+from resources.clients import ClientObjectResource, ClientListResource
+api.add_resource(ClientListResource,'/clients')
+api.add_resource(ClientObjectResource, '/clients/<int:id>')
 
-
-
-from resources.branches import BranchResource
+from resources.branches import BranchObjectResource, BranchListResource
 #   ,'/banks/<int:id>/branches'
-api.add_resource(BranchResource,'/banks/branches/<int:id>')
+api.add_resource(BranchObjectResource,'/banks/branches/<int:id>')
 
-from resources.accounts import AccountResource
-#   , '/banks/<int:id>/accounts'
-#   , '/clients/<int:id>/accounts'
-#   , '/clients/<int:id>/accounts/<int:id>'
-api.add_resource(AccountResource, '/banks/accounts/<int:id>')
+from resources.accounts import AccountObjectResource, AccountListResource
+# #   , '/banks/<int:id>/accounts'
+# #   , '/clients/<int:id>/accounts'
+# #   , '/clients/<int:id>/accounts/<int:id>'
+api.add_resource(AccountObjectResource, '/banks/accounts/<int:id>')
 
-from resources.cards import CardResource
+from resources.cards import CardObjectResource, CardListResource
 #   , '/accounts/<int:id>/cards'
-api.add_resource(CardResource, '/accounts/cards/<int:id>')
+api.add_resource(CardObjectResource, '/accounts/cards/<int:id>')
+
+app.register_blueprint(blueprint, url_prefix="/api")
 
 if __name__ == '__main__':
     app.debug = False
