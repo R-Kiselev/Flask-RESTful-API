a43806f3e8b7e8f09bacd8f9fe87aa49fad775ed
R-Kiselev <sputnavik@gmail.com>
2024-10-16 15:35:26 +0300

    fix: client_id and bank_id are loadable now

--- app/api/schemas/account.py
+++ app/api/schemas/account.py
@@ -4,8 +4,8 @@ from marshmallow import validates, ValidationError
 class AccountSchema(Schema):
     id = fields.Int(dump_only= True)
     balance = fields.Int()
-    client_id = fields.Int(required = True, dump_only = True)
-    bank_id = fields.Int(required = True, dump_only = True)
+    client_id = fields.Int(required = True)
+    bank_id = fields.Int(required = True)
 
     @validates("balance")
     def validate_balance(self, value):
