84d9ee9258c9e64772c18193a8dfc4cbe768c5bb
R-Kiselev <sputnavik@gmail.com>
2024-12-07 00:16:24 +0300

    feat: added `user_id` in `ClientSchema`

--- app/api/schemas/client.py
+++ app/api/schemas/client.py
@@ -6,11 +6,17 @@ class ClientSchema(Schema):
     id = fields.Int(dump_only= True)
     name = fields.Str(required= True)
     social_status_id = fields.Int(required = True)
-    
+    user_id = fields.Int(required = True)
+
     @validates("name")
     def validate_name(self, value):
         validate_name(value)
     
     @validates("social_status_id")
     def validate_social_status_id(self, value):
+        validate_id(value)
+
+    
+    @validates("user_id")
+    def validate_user_id(self, value):
         validate_id(value)
