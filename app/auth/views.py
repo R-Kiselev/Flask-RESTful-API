b5c38be823cf14a9b691be61245a0555698cecc8
R-Kiselev <sputnavik@gmail.com>
2024-12-07 00:14:11 +0300

    refactor: moved `user lookup loader` into utils.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,58 +5,67 @@ from flask import (
 )
 from flask_jwt_extended import (
     create_access_token,
-    create_refresh_token,
     jwt_required,
-    get_jwt_identity,
     get_current_user,
     get_jwt
 )
 
 from app.extensions import jwt, pwd_context
-from app.api.resources.users import UserListResource
 from app.models.user import User
+from app.auth.utils import user_roles_required
+from app.api.schemas.user import GetUserSchema
 
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @blueprint.route('/register', methods = ['POST'])
 def register():
-    try:
-        response = UserListResource().post()
-        return response
-    except Exception as e:
-        return jsonify({"error" : str(e)}), 400
+    from app.api.resources.users import UserListResource
+
+    return UserListResource().post() 
 
 
 @blueprint.route('/login', methods = ['POST'])
 def login():
-    req = request.json
-    email = req.get('email', None)
-    password = req.get('password', None)
+    request_data = request.json
+    email = request_data.get('email')
+    password = request_data.get('password')
 
     if not email or not password:
         return jsonify({'msg' : 'Missing required fields'}), 400
 
     user = User.query.filter_by(email = email).first()
-    if user is None or not pwd_context.verify(password, user.password):
+    if not user or not pwd_context.verify(password, user.password):
         return jsonify({'msg' : 'Bad credentials'}), 400
+    
+    if (user.is_blocked):
+        return jsonify({'err' : 'Access denied. You are blocked'}), 401
     
     access_token = create_access_token(identity=user.id)
 
-    return jsonify({'access_token' : access_token}), 200
-
-
-@jwt.user_lookup_loader
-def user_lookup_callback(_jwt_header, jwt_payload):
-    identity = jwt_payload["sub"]
-    return User.query.get(identity)
+    return jsonify({'access_token' : access_token}), 200 
 
 
 @blueprint.route('/whoami', methods = ['GET'])
 @jwt_required()
 def whoami():
     current_user = get_current_user()
-    return jsonify({
-        'email' : current_user.email,
-        'roles' : [role.name for role in current_user.roles]
-    })
+
+    token = get_jwt()
+    claims = {
+        'client_id' : token.get('client_id'),
+        'account_ids' : token.get('account_ids')
+    }
+
+    return {
+        'user' : GetUserSchema().dump(current_user),
+        'token_claims' : claims
+    }, 200
+
+
+@blueprint.route('/admin_required', methods = ['GET'])
+@jwt_required()
+@user_roles_required('admin')
+def admin_required():
+    current_user = get_current_user()
+    return GetUserSchema().dump(current_user), 200
