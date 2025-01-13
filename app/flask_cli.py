4b9a55c55fc66e5ac59bc245f5b4ba4075fab295
R-Kiselev <sputnavik@gmail.com>
2025-01-13 16:03:48 +0300

    feat: add role creation command to flask CLI command `init`

--- app/flask_cli.py
+++ app/flask_cli.py
@@ -4,15 +4,29 @@ import click
 
 from flask.cli import with_appcontext
 
+from app.extensions import db
+
 
 dotenv.load_dotenv()
 
 
-@click.command('init')
-@with_appcontext
-def init():
+def create_roles():
+    from app.models.role import Role
+
+    roles = ['admin', 'user']
+
+    click.echo('Creating roles')
+    for role in roles:
+        if not Role.query.filter_by(name=role).first():
+            db.session.add(Role(name=role))
+        else:
+            click.echo('Roles already exists')
+            return
+    db.session.commit()
+
+
+def create_admin():
     """Create a new admin user"""
-    from app.extensions import db
     from app.models.user import User
     from app.api.schemas.user import UserSchema
 
@@ -23,6 +37,8 @@ def init():
     }
 
     data = UserSchema().load(admin_info)
+
+    click.echo('Creating admin user')
     if (User.query.filter_by(email=data.get('email', None)).first()):
         click.echo('Admin already exists')
         return
@@ -31,3 +47,10 @@ def init():
 
     db.session.add(admin)
     db.session.commit()
+
+
+@click.command('init')
+@with_appcontext
+def init():
+    create_roles()
+    create_admin()
