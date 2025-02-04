a6ed1a4c7e7247cb5bc79ceecbcabc120fd17588
R-Kiselev <sputnavik@gmail.com>
2025-02-05 00:01:20 +0300

    feat(bank-servie): added message queue simple publisher

---
+++ bank-service/app/message_queue.py
@@ -0,0 +1,24 @@
+import json
+from pika import BlockingConnection, ConnectionParameters
+
+
+class MessageQueue:
+    connection_params = ConnectionParameters(
+        host='rabbitmq',
+        port=5672
+    )
+
+    def send_message_to_queue(self, message: str, queue: str):
+        with BlockingConnection(self.connection_params) as connection:
+            with connection.channel() as channel:
+                message_json = json.dumps(message)
+
+                channel.queue_declare(queue=queue, durable=True)
+
+                channel.basic_publish(
+                    exchange='',
+                    routing_key=queue,
+                    body=message_json
+                )
+
+                print(f"Sent: {message}")
