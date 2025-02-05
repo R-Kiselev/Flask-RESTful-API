8656dbcb2c1ed984bd1d172baa35432424d2b420
R-Kiselev <sputnavik@gmail.com>
2025-02-05 15:18:13 +0300

    feat(log-service): enhance message processing and queue consumption logic

--- log-service/app/message_queue.py
+++ log-service/app/message_queue.py
@@ -1,5 +1,6 @@
 import asyncio
-from aio_pika import connect
+from typing import Optional, Callable
+from aio_pika import connect, Connection, Queue
 from aio_pika.abc import AbstractIncomingMessage
 from aio_pika.exceptions import AMQPError
 
@@ -9,7 +10,7 @@ from .logging import logger
 RABBITMQ_URL = str(settings.amqp_dsn)
 
 
-async def on_message(message: AbstractIncomingMessage) -> None:
+async def on_message(message: AbstractIncomingMessage) -> Optional[str]:
     try:
         decoded_message = message.body.decode()
         logger.info(f'Processed message content: {decoded_message}')
@@ -17,10 +18,22 @@ async def on_message(message: AbstractIncomingMessage) -> None:
         await message.ack()
         logger.info('Message acknowledged')
 
+        return decoded_message
     except Exception as e:
         logger.error(f'Error processing message: {e}')
         message.nack(requeue=True)
         logger.warning('Message nack with requeue')
+
+        return None
+
+
+async def consume_queue(queue: Queue, on_message: Callable) -> None:
+    async with queue.iterator() as queue_iter:
+        async for message in queue_iter:
+            decoded_message = await on_message(message)
+            if decoded_message is None:
+                continue
+            logger.info(f'listener: {decoded_message}')
 
 
 async def listen() -> None:
@@ -33,9 +46,10 @@ async def listen() -> None:
                 queue = await channel.declare_queue('log-service', durable=True)
 
                 logger.info('Waiting for messages.')
-                await queue.consume(on_message, no_ack=False)
+                await consume_queue(queue, on_message)
 
-                await asyncio.Future()
+                # await queue.consume(on_message, no_ack=False)
+                # await asyncio.Future()
 
         except AMQPError as e:
             logger.error(f'RabbitMQ connection error: {e}')
