daa3b9ff088cb64d1f9d1fb20287082f108535a5
R-Kiselev <sputnavik@gmail.com>
2025-02-07 14:34:02 +0300

    feat(log-service): set cleaner message listener name and added log router

--- log-service/app/main.py
+++ log-service/app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 
 from .db import connect_and_init_db, close_db_connection, get_db_client
-from .message_queue import start_message_listener
-
+from .message_queue import start_messages_saver
+from .resources.logs import router as logs_router
 
 app = FastAPI()
 
 app.add_event_handler("startup", connect_and_init_db)
 app.add_event_handler("shutdown", close_db_connection)
-app.add_event_handler("startup", start_message_listener)
-
+app.add_event_handler("startup", start_messages_saver)
+app.include_router(logs_router)
 
 @app.get("/")
 async def root():
