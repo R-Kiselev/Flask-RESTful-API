e35ad9cee8498bdaad1600f5e17a2830cce0a131
R-Kiselev <sputnavik@gmail.com>
2025-02-15 23:56:43 +0300

    refactor(bank-service): initialize MessageQueue client in extensions

--- bank-service/app/extensions.py
+++ bank-service/app/extensions.py
@@ -3,7 +3,12 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from passlib.context import CryptContext
 
+from app.message_queue import MessageQueue
+
+
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+
+message_queue_client = MessageQueue()
