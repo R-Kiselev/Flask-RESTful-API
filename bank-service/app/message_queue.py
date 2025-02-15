92b668f230df32188366e24d42c53cf18e1247dd
R-Kiselev <sputnavik@gmail.com>
2025-02-15 23:56:05 +0300

    refactor(bank-service): improve message queue handling and connection management

--- bank-service/app/message_queue.py
+++ bank-service/app/message_queue.py
@@ -1,6 +1,6 @@
 import json
 import uuid
-import time 
+import time
 
 from pika import BlockingConnection, ConnectionParameters, BasicProperties
 
@@ -11,12 +11,12 @@ RABBIT_REPLY_QUEUE = "amq.rabbitmq.reply-to"
 
 
 class MessageQueueConnectionManager:
-    def __init__(self, 
+    def __init__(self,
                  host: str = RABBITMQ_HOST,
                  port: int = RABBITMQ_PORT,
-                 retries: int = 2, 
+                 retries: int = 2,
                  interval: int = 3
-                ) -> None:
+                 ) -> None:
         self.host = host
         self.port = port
         self.retries = retries
@@ -26,7 +26,7 @@ class MessageQueueConnectionManager:
 
     def connect(self):
         retries = 0
-        
+
         while True:
             try:
                 self.connection = BlockingConnection(
@@ -62,34 +62,39 @@ class MessageQueue:
                  exchange_type: str = 'topic',
                  reply_queue: str = RABBIT_REPLY_QUEUE,
                  timeout: int = 2
-                ):
+                 ):
         self.exchange_name = exchange_name
         self.exchange_type = exchange_type
         self.reply_queue = reply_queue
         self.timeout = timeout
+        self.connection_manager = MessageQueueConnectionManager()
 
+    def connect(self):
+        self.connection, self.channel = self.connection_manager.connect()
+
+    def close(self):
+        self.connection_manager.close()
+
+    def setup_mq(self):
         self.connect()
         self.channel.exchange_declare(
             exchange=self.exchange_name,
             exchange_type=self.exchange_type
         )
 
-    def connect(self):
-        self.connection, self.channel = MessageQueueConnectionManager().connect()
-
     def on_reply(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             # Assigning the response is crucial part of the consuming process
-            # Until responce is none and timeout is not reached, the main thread will be blocked 
+            # Until responce is none and timeout is not reached, the main thread will be blocked
             self.response = body
             self.delivery_tag = method.delivery_tag
 
     def setup_reply_queue(self):
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_consume(
-            queue = self.reply_queue,
-            on_message_callback = self.on_reply,
-            auto_ack = True
+            queue=self.reply_queue,
+            on_message_callback=self.on_reply,
+            auto_ack=True
         )
 
     def get_reply_message(self):
@@ -103,32 +108,35 @@ class MessageQueue:
                 print("No reply from server")
 
                 return None
-            
+
         self.channel.basic_ack(delivery_tag=self.delivery_tag)
         print("received:", self.response)
 
         return self.response.decode()
 
     def send_message(self, message: dict, routing_key: str, need_reply: bool = False):
-        message = json.dumps(message)
+        self.setup_mq()
 
-        if need_reply:
-            self.setup_reply_queue()
+        with self.channel, self.connection:
+            message = json.dumps(message)
 
-        self.channel.basic_publish(
-            exchange=self.exchange_name,
-            routing_key=routing_key,
-            body=message.encode(),
+            if need_reply:
+                self.setup_reply_queue()
 
-            # If need_reply is True, then set reply_to and correlation_id
-            properties=BasicProperties(
-                reply_to=self.reply_queue,
-                correlation_id=self.corr_id
-            ) if need_reply else None
-        )
-        print("sent:", message)
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=routing_key,
+                body=message.encode(),
 
-        if need_reply:
-            return self.get_reply_message()
-        
-        return None
+                # If need_reply is True, then set reply_to and correlation_id
+                properties=BasicProperties(
+                    reply_to=self.reply_queue,
+                    correlation_id=self.corr_id
+                ) if need_reply else None
+            )
+            print("sent:", message)
+
+            if need_reply:
+                return self.get_reply_message()
+
+            return None
